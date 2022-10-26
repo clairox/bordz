cf3ae5f49b06a5cf2b09f76ffe9702d825a33ce0
Claire Q <claireq620@gmail.com>
2022-10-26 15:47:58 -0400

    Logging result of product query

--- pages/api/products/[pid]/index.ts
+++ pages/api/products/[pid]/index.ts
@@ -11,6 +11,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 				where: { id: parseInt(pid) },
 			})
 			.then(product => {
+				console.log(product);
 				if (product) {
 					return res.status(200).json({ ...product, price: parseInt(product.price.toString()) });
 				}
