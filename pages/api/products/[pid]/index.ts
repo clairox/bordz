3b7cd5271a8a2d910723aee602bc694ffd1f2ad3
Claire Q <claireq620@gmail.com>
2022-12-08 17:03:05 -0500

    Testing

--- pages/api/products/[pid]/index.ts
+++ pages/api/products/[pid]/index.ts
@@ -1,19 +1,23 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-	if (prisma === undefined) return res.status(500).json(null);
+	if (prisma === undefined) return res.status(400).json(null);
 
 	if (req.method === 'GET') {
 		const { pid } = req.query as { [key: string]: string };
 
-		const product = await prisma.product.findUnique({
-			where: { id: parseInt(pid) },
-		});
-
-		if (product) {
-			return res.status(200).json({ ...product, price: parseInt(product.price.toString()), salePrice: parseInt(product.salePrice.toString()) });
-		}
-		return res.status(400).json({});
+		await prisma.product
+			.findUnique({
+				where: { id: parseInt(pid) },
+			})
+			.then(product => {
+				if (product) {
+					return res
+						.status(200)
+						.json({ ...product, price: parseInt(product.price.toString()), salePrice: parseInt(product.salePrice.toString()) });
+				}
+				return res.status(400).json({});
+			});
 	} else {
 		return res.status(405).json(null);
 	}
