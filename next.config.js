b17ea9a56ae40252993f53fef6bfc1506987b180
Claire Q <claireq620@gmail.com>
2022-12-08 13:44:26 -0500

    Edited next config

--- next.config.js
+++ next.config.js
@@ -3,12 +3,14 @@ const nextConfig = {
 	reactStrictMode: false,
 	swcMinify: true,
 	images: {
+		loader: 'akamai',
 		domains: ['scene7.zumiez.com'],
 	},
 	i18n: {
 		locales: ['en'],
 		defaultLocale: 'en',
 	},
+	assetPrefix: './',
 };
 
 module.exports = nextConfig;
