57bdf811541a8a1b2bbb8578101a75fc1972f6f5
Claire Q <claireq620@gmail.com>
2022-12-08 16:29:42 -0500

    Moved stuff around

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,8 +4,8 @@ declare global {
 	var prisma: PrismaClient | undefined;
 }
 
-const prisma = global.prisma || new PrismaClient();
-
-export default prisma;
+const prisma = global.prisma || new PrismaClient({ log: ['error', 'query'] });
 
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+
+export default prisma;
