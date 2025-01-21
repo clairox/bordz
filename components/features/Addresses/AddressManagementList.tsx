22a85ea44fae4c95a325a79d398b01dd193b541b
clairox <claireq620@gmail.com>
2025-01-21 09:43:05 -0500

    feat: alert user on address deletion

--- components/features/Addresses/AddressManagementList.tsx
+++ components/features/Addresses/AddressManagementList.tsx
@@ -6,6 +6,18 @@ import { Plus } from '@phosphor-icons/react'
 import { Button } from '@/components/ui/Button'
 import { useDeleteAddress } from '@/hooks/data/address'
 import { AddressCard } from './AddressCard'
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from '@/components/ui/AlertDialog'
+import React from 'react'
 
 type AddressManagementListProps = {
     addresses: Address[]
@@ -13,8 +25,6 @@ type AddressManagementListProps = {
 export const AddressManagementList: React.FC<AddressManagementListProps> = ({
     addresses,
 }) => {
-    const { mutateAsync: deleteAddress } = useDeleteAddress()
-
     if (addresses.length > 0) {
         return (
             <div className="pl-8 w-1/2">
@@ -22,11 +32,7 @@ export const AddressManagementList: React.FC<AddressManagementListProps> = ({
                     <NewAddressButton />
                 </div>
                 {addresses.map(address => (
-                    <AddressManagementCard
-                        address={address}
-                        deleteAddress={id => deleteAddress({ id })}
-                        key={address.id}
-                    />
+                    <AddressManagementCard address={address} key={address.id} />
                 ))}
             </div>
         )
@@ -44,27 +50,62 @@ export const AddressManagementList: React.FC<AddressManagementListProps> = ({
 
 type AddressManagementCard = {
     address: Address
-    deleteAddress: (id: string) => Promise<void>
 }
 
 const AddressManagementCard: React.FC<AddressManagementCard> = ({
     address,
-    deleteAddress,
 }) => {
     const router = useRouter()
 
     const handleEditNavigation = () =>
         router.push(`/account/addresses/edit/${address.id}`)
-    const handleDeleteAddress = () => deleteAddress(address.id)
 
     return (
         <div key={address.id} className="flex justify-between items-start mb-3">
             <AddressCard address={address} />
             <div className="flex gap-2">
                 <button onClick={handleEditNavigation}>Edit</button>
-                <button onClick={handleDeleteAddress}>Delete</button>
+                <DeleteAddressButton address={address} />
             </div>
         </div>
+    )
+}
+
+type DeleteAddressButtonProps = {
+    address: Address
+}
+
+const DeleteAddressButton: React.FC<DeleteAddressButtonProps> = ({
+    address,
+}) => {
+    const deleteAddress = useDeleteAddress()
+
+    return (
+        <AlertDialog>
+            <AlertDialogTrigger>Delete</AlertDialogTrigger>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>Delete address?</AlertDialogTitle>
+                    <AlertDialogDescription asChild>
+                        <div>
+                            Are you sure you want to delete the following
+                            address:
+                            <div className="mt-2">
+                                <AddressCard address={address} />
+                            </div>
+                        </div>
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <AlertDialogAction
+                        onClick={() => deleteAddress.mutate({ id: address.id })}
+                    >
+                        Delete
+                    </AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
     )
 }
 
