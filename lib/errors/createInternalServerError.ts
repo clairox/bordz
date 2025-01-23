1cf4c30e3275b2731ad6b55047a76057f585d5a0
clairox <claireq620@gmail.com>
2025-01-23 10:40:26 -0500

    refactor: enforce default message

--- lib/errors/createInternalServerError.ts
+++ lib/errors/createInternalServerError.ts
@@ -1,6 +1,7 @@
+import { UNEXPECTED_ERROR_TEXT } from '@/utils/constants'
 import APIError from '../APIError'
 
-const createInternalServerError = (message?: string) => {
+const createInternalServerError = (message: string = UNEXPECTED_ERROR_TEXT) => {
     return new APIError(
         500,
         `Internal server error${message ? ': ' + message : '.'}`,
